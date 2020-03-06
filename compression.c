00f38f7181880895c40a79dd4a370afb88892ad8
Brian Irvine <brianirvine@Brians-MacBook-Pro.local>
2020-03-06 13:50:11 +0200

    first commit

---
+++ compression.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+  int data_size, new_size;
+  short int buffer[];
+  int *data_pnt;
+
+  // example buffer and data_size
+  buffer[] = {0}
+
+  data_pnt = &buffer;
+
+  data_size = 24;
+  buffer = {0x03, 0x07}
+  // Example initial array
+
+  return siz
+}
